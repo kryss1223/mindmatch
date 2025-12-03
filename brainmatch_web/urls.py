d1149c1e384fa5353e7d2ffb868a2812c9970e3e
Cristina Santos <128757175+kryss1223@users.noreply.github.com>
2025-12-03 19:46:44 +0100

    Update urlpatterns to use relative paths
    Removed full URLs from urlpatterns and replaced with empty paths.

--- brainmatch_web/urls.py
+++ brainmatch_web/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('https://mindmatch-76oz.onrender.com/', include('users.urls')),
-    path('https://mindmatch-76oz.onrender.com/', include('projects.urls')),
-    path('https://mindmatch-76oz.onrender.com/', include('ideas.urls')),
+    path('', include('users.urls')),
+    path('', include('projects.urls')),
+    path('', include('ideas.urls')),
+
 ]
